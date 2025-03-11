e37979b3289d4002448758cb27e8a5feed2a1a3d
fazlerabbi13 <mohammad0132794@gmail.com>
2025-03-12 00:44:05 +0600

    taken multiple input

---
+++ MultipleInput.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class MultipleInput {
+    public static void main(String[] args) {
+        Scanner dd = new Scanner(System.in);
+
+        int a = dd.nextInt();
+        int b = dd.nextInt();
+        int c = dd.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
