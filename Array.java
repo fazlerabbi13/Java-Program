a3fc523c30c2a4e6b33f22a8ddd9a130199aab27
fazlerabbi13 <mohammad0132794@gmail.com>
2025-03-13 18:38:10 +0600

    loop through an Array

---
+++ Array.java
@@ -0,0 +1,8 @@
+public class Array {
+    public static void main(String[] args) {
+        String[] cars = {"toyota","prado","BMW","Marcidiz"};
+        for (int i = 0; i < cars.length; i++) {
+         System.out.println(cars[i]);
+         }
+    }
+}
