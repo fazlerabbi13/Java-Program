285599ed68c2623a53662ebc7a43c925204f2237
fazlerabbi13 <mohammad0132794@gmail.com>
2025-03-14 15:12:10 +0600

    calling a method with passing perameter

---
+++ MethodCall.java
@@ -0,0 +1,11 @@
+public class MethodCall {
+    static void myMethod(String name){
+        System.out.println(name + " " + "Khan");
+    }
+
+    public static void main(String[] args) {
+        myMethod("ali");
+        myMethod("sakib");
+        myMethod("sagor");
+    }
+}
