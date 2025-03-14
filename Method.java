59b8d8956b31b5eb8e24b239ea97516b62d18015
fazlerabbi13 <mohammad0132794@gmail.com>
2025-03-14 15:07:15 +0600

    calling a method

---
+++ Method.java
@@ -0,0 +1,10 @@
+
+public class Method {
+    static void myMethod(){
+        System.out.println("Hello world");
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+    }
+}
