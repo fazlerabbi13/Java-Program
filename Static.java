9a642dd32a19e418d1b77f80c0f8519988ae79b6
fazlerabbi13 <mohammad0132794@gmail.com>
2025-03-23 21:53:14 +0600

    A static method means that it can be accessed without creating an object of the class, unlike public

---
+++ Static.java
@@ -0,0 +1,19 @@
+public class Static {
+    // Static method
+  static void myStaticMethod() {
+    System.out.println("Static methods can be called without creating objects");
+  }
+
+  // Public method
+  public void myPublicMethod() {
+    System.out.println("Public methods must be called by creating objects");
+  }
+
+  // Main method
+  public static void main(String[] args) {
+    myStaticMethod(); // Call the static method
+
+    Static myObj = new Static(); // Create an object of MyClass
+    myObj.myPublicMethod(); // Call the public method
+  }
+}
