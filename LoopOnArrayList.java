8af02c688df80fccf72566f261921d7e6e41f88f
fazlerabbi13 <mohammad0132794@gmail.com>
2025-03-31 20:51:48 +0600

    To modify an element, use the set() method and refer to the index number

--- LoopOnArrayList.java
+++ LoopOnArrayList.java
@@ -9,11 +9,12 @@ public class LoopOnArrayList {
     cars.add("Mazda");
 
     // access item
-   System.out.println( cars.get(1));
-
-    for (int i = 0; i < cars.size(); i++) {
-      System.out.println(cars.get(i));
-    }
+  //  System.out.println( cars.get(1));
+      cars.set(1,"Toyota");
+      System.out.println(cars);
+    // for (int i = 0; i < cars.size(); i++) {
+    //   System.out.println(cars.get(i));
+    // }
 
 
   }
