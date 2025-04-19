6fbbf54a70d2be13b693252618e533fe34afab5a
fazlerabbi13 <mohammad0132794@gmail.com>
2025-04-19 13:25:59 +0600

    The iterator() method can be used to get an Iterator for any collection

---
+++ iterator.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class iterator {
+     public static void main(String[] args) {
+  
+    // Make a collection
+    ArrayList<String> cars = new ArrayList<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+  
+    // Get the iterator
+    Iterator<String> it = cars.iterator();
+  
+    // Print the first item
+    Syste
+}
