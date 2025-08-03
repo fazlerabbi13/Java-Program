0699f451ff5b826a2b3539e84a2d1136ecce5b18
fazlerabbi13 <mohammad0132794@gmail.com>
2025-08-03 09:29:46 +0600

    making arraylist adding elements.

---
+++ ArrayList1.java
@@ -0,0 +1,18 @@
+
+import java.util.ArrayList;
+
+class ArrayList1 {
+    public static void main (String[] args) {
+        
+      	ArrayList <Integer> array = new ArrayList <Integer>();
+
+        array.add(1);
+        array.add(2);
+        array.add(3);
+        array.add(4);
+        array.add(5);
+
+        System.out.println(array);
+      	
+    }
+}
